97081df46743ae6e2d5248dd9264096e597a23fb
Samir Chaudhry <schaudhry@zscaler.com>
2014-07-20 17:14:23 -0700

    fixing 405 error

--- process.php
+++ process.php
@@ -1,4 +1,14 @@
 <?php
-	mail('schaudhry123@gmail.com', 'subject', 'message');
+	require 'vendor/autoload.php';
+
+	$sendgrid = new SendGrid('name', 'email');
+	$email    = new SendGrid\Email();
+	$email->addTo('schaudhry123@gmail.com')->
+  	setFrom('schaudhry123@gmail.com')->
+  	setSubject('Zscaler Vacation Form Submission')->
+   	setText('Hello World!')->
+   	setHtml('<strong>Hello World!</strong>');
+
+	$sendgrid->send($email);
 ?>
 <p>Your email has been sent.</p>
